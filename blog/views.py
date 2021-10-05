e275e2fbe5392303db20e79f8132f476320f1cda
Maneth21 <manethyethmin22@gmail.com>
2021-10-05 20:18:50 +0530

    Updated templates

--- blog/views.py
+++ blog/views.py
@@ -1,93 +1,102 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.utils import timezone, dateformat
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 
 from .models import Article
-print()
+
 
 def index(request):
-	if request.user.is_authenticated:
-		return HttpResponseRedirect('/dashboard')
-	return render(request, 'pages/home.html')
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/dashboard')
+    return render(request, 'pages/home.html')
+
 
 def dashboard(request):
-	articles = []
-	if not request.user.is_authenticated:
-		return HttpResponseRedirect('/auth/login')
-	try:
-		user = User.objects.get(username=request.user.username)
-		articles = user.article_set.all()
-	except:
-		pass
-	return render(request, 'pages/dashboard.html', {'articles': articles})
+    articles = []
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/auth/login')
+    try:
+        user = User.objects.get(username=request.user.username)
+        articles = user.article_set.all()
+    except Exception as e:
+        print(e)
+    return render(request, 'pages/dashboard.html', {'articles': articles})
+    # return HttpResponse('Hello')
+
 
 def allArticles(request):
-	try:
-		articles = Article.objects.all()
-		return render(request, 'articles/all.html', {'articles': articles})
-	except Exception as e:
-		print(e)
-		return render(request, 'articles/all.html')
+    try:
+        articles = Article.objects.all()
+        return render(request, 'articles/all.html', {'articles': articles})
+    except Exception as e:
+        print(e)
+        return render(request, 'articles/all.html')
+
 
 def singleArticle(request, id):
-	try:
-		article = Article.objects.get(pk=id)
-		article.views += 1
-		article.save()
-		return render(request, 'articles/single.html', {'article': article})
-	except:
-		pass
+    try:
+        article = Article.objects.get(pk=id)
+        article.views += 1
+        article.save()
+        return render(request, 'articles/single.html', {'article': article})
+    except:
+        pass
+
 
 def newArticle(request):
-	if request.method == 'POST':
-		user = User.objects.get(username=request.user.username)
+    if request.method == 'POST':
+        user = User.objects.get(username=request.user.username)
 
-		title = request.POST['title']
-		description = request.POST['description']
-		body = request.POST['body']
+        title = request.POST['title']
+        description = request.POST['description']
+        body = request.POST['body']
 
-		article = Article.objects.create(user=user, title=title, description=description, body=body)
-		article.save()
+        article = Article.objects.create(
+            user=user, title=title, description=description, body=body)
+        article.save()
 
-		return HttpResponseRedirect('/dashboard')
+        return HttpResponseRedirect('/dashboard')
 
-	else:
-		return render(request, 'articles/new.html')
+    else:
+        return render(request, 'articles/new.html')
+
 
 def editArticle(request, id):
-	if request.method == 'POST':
-		title = request.POST['title']
-		description = request.POST['description']
-		body = request.POST['body']
+    if request.method == 'POST':
+        title = request.POST['title']
+        description = request.POST['description']
+        body = request.POST['body']
 
-		try:
-			article = Article.objects.get(pk=id)
-			article.title = title
-			article.description = description
-			article.body = body
-			article.save()
+        try:
+            article = Article.objects.get(pk=id)
+            article.title = title
+            article.description = description
+            article.body = body
+            article.save()
 
-			return HttpResponseRedirect('/dashboard')
-		except:
-			return HttpResponseRedirect('/dashboard')
-		
-	else:
-		try:
-			article = Article.objects.get(pk=id)
-			if not article.user.username == request.user.username:
-				return HttpResponseRedirect('/articles')
-		except:
-			return HttpResponseRedirect('/dashboard')
-		return render(request, 'articles/edit.html' , {'values': article})
+            return HttpResponseRedirect('/dashboard')
+        except:
+            return HttpResponseRedirect('/dashboard')
+
+    else:
+        try:
+            article = Article.objects.get(pk=id)
+            if not article.user.username == request.user.username:
+                return HttpResponseRedirect('/articles')
+        except:
+            return HttpResponseRedirect('/dashboard')
+        return render(request, 'articles/edit.html', {'values': article})
+
 
 def deleteArticle(request, id):
-	article = Article.objects.get(pk=id)
-	if not article.user.username == request.user.username:
-				return HttpResponseRedirect('/articles')
-	article.delete()
+    article = Article.objects.get(pk=id)
+    if not article.user.username == request.user.username:
+        return HttpResponseRedirect('/articles')
+    article.delete()
 
-	return HttpResponseRedirect('/dashboard')
+    return HttpResponseRedirect('/dashboard')
+
 
 def commentArticle(request, id):
-	return HttpResponseRedirect('/')
+    return HttpResponseRedirect('/')
